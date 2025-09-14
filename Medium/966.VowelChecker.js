771c18ceb818daaf9c5b6301f4de5ce3316fee90
Shreyas5706 <shreyassolanki516@gmail.com>
2025-09-14 08:37:23 +0530

    966

---
+++ Medium/966.VowelChecker.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string[]} wordlist
+ * @param {string[]} queries
+ * @return {string[]}
+ */
+var spellchecker = function(wordlist, queries) {
+    const exact = new Set(wordlist);
+    const lowerMap = new Map();
+    const vowelMap = new Map();
+    for (let w of wordlist) {
+        const wl = w.toLowerCase();
+        if (!lowerMap.has(wl)) lowerMap.set(wl, w);
+        const m = maskVowels(wl);
+        if (!vowelMap.has(m)) vowelMap.set(m, w);
+    }
+
+    const ans = [];
+    for (let q of queries) {
+        if (exact.has(q)) {
+            ans.push(q);
+            continue;
+        }
+        const ql = q.toLowerCase();
+        if (lowerMap.has(ql)) {
+            ans.push(lowerMap.get(ql));
+            continue;
+        }
+        const qm = maskVowels(ql);
+        ans.push(vowelMap.get(qm) || "");
+    }
+    return ans;
+};
+function maskVowels(s){
+    s=s.toLowerCase();
+    let out ='';
+    for(let ch of s){
+        out+= (isVowel(ch)?'*':ch);
+    }
+    return out;
+
+}
+function isVowel(c){
+    return "aeiou".includes(c);
+}
