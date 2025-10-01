96132e489985370c542312a6d6d5e35bc1372189
Shreyas5706 <shreyassolanki516@gmail.com>
2025-10-01 10:27:29 +0530

    1518

---
+++ Easy/1518.WaterBottles.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function(numBottles, numExchange) {
+    let total = numBottles;
+    let empties = numBottles;
+
+    while (empties >= numExchange) {
+        let newFull = Math.floor(empties / numExchange);
+        total += newFull;
+        empties = newFull + (empties % numExchange);
+    }
+    return total;
+};
