c9a39cace36268f34708e8c78dbba702f4ec5bca
Shreyas5706 <shreyassolanki516@gmail.com>
2025-10-05 09:30:42 +0530

    417

---
+++ Medium/417.PacificAtlanticWaterFlow.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+ var pacificAtlantic = function(heights) {
+    const numRows = heights.length;
+    const numCols = heights[0].length;
+
+    const pacific = Array(numRows).fill().map(() => Array(numCols).fill(false));
+    const atlantic = Array(numRows).fill().map(() => Array(numCols).fill(false));
+
+    for (let col = 0; col < numCols; col++) {
+        dfs(0, col, -Infinity, pacific);
+        dfs(numRows - 1, col, -Infinity, atlantic);
+    }
+
+    for (let row = 0; row < numRows; row++) {
+        dfs(row, 0, -Infinity, pacific);
+        dfs(row, numCols - 1, -Infinity, atlantic);
+    }
+
+    function dfs(i, j, prev, ocean) {
+        if (
+            i < 0 ||
+            i >= numRows ||
+            j < 0 ||
+            j >= numCols ||
+            heights[i][j] < prev ||
+            ocean[i][j]
+        ) {
+            return;
+        }
+
+        ocean[i][j] = true;
+
+        dfs(i + 1, j, heights[i][j], ocean);
+        dfs(i - 1, j, heights[i][j], ocean);
+        dfs(i, j + 1, heights[i][j], ocean);
+        dfs(i, j - 1, heights[i][j], ocean);
+    }
+
+    const result = [];
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            if (pacific[i][j] && atlantic[i][j]) {
+                result.push([i, j]);
+            }
+        }
+    }
+    return result;
+};
