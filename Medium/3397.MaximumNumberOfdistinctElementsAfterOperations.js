9dcec92ed5782d5c75f049a274ade01c602e863b
Shreyas5706 <shreyassolanki516@gmail.com>
2025-10-18 22:03:40 +0530

    3397

---
+++ Medium/3397.MaximumNumberOfdistinctElementsAfterOperations.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxDistinctElements = function(nums, k) {
+    nums.sort((a, b) => a - b);  // Sort the array
+    let count = 0;
+    let prev = -Infinity;
+    
+    for (let num of nums) {
+        // Determine the smallest valid selectable value for this element
+        let low = Math.max(num - k, prev + 1);
+        let chosen = Math.min(low, num + k);
+
+        if (chosen > prev) {
+            count++;
+            prev = chosen;
+        }
+    }
+    return count;
+};
