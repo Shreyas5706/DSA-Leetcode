31f05b522a0865101b31443691b7915d24ddfbfc
Shreyas5706 <shreyassolanki516@gmail.com>
2025-10-26 21:38:35 +0530

    2043

---
+++ Medium/2043.SimpleBankSystem.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} balance
+ */
+var Bank = function(balance) {
+    this.balance = balance;
+};
+
+/** 
+ * @param {number} account1 
+ * @param {number} account2 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.transfer = function(account1, account2, money) {
+    if (
+        account1 < 1 || account1 > this.balance.length ||
+        account2 < 1 || account2 > this.balance.length
+    ) return false;
+    if (this.balance[account1 - 1] < money) return false;
+    this.balance[account1 - 1] -= money;
+    this.balance[account2 - 1] += money;
+    return true;
+};
+
+/** 
+ * @param {number} account 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.deposit = function(account, money) {
+    if (account < 1 || account > this.balance.length) return false;
+    this.balance[account - 1] += money;
+    return true;
+};
+
+/** 
+ * @param {number} account 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.withdraw = function(account, money) {
+    if (account < 1 || account > this.balance.length) return false;
+    if (this.balance[account - 1] < money) return false;
+    this.balance[account - 1] -= money;
+    return true;
+};
