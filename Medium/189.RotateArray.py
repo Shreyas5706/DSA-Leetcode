5d8715378303a0ec900804bf6a3782f3118835c4
Shreyas5706 <shreyassolanki516@gmail.com>
2026-08-08 15:53:57 +0530

    189.Rotate Array

---
+++ Medium/189.RotateArray.py
@@ -0,0 +1,30 @@
+#1. Space efficient approach
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        def reverse(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        reverse(0,len(nums)-1)
+        reverse(0,k-1)
+        reverse(k,len(nums)-1)
+        
+#2. Time efficient approach
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        nums[:]=nums[-k:]+nums[:-k]
+        
