cc2d123b772a84d1e6bef36e18e8b4bcf03fd41f
Shreyas5706 <shreyassolanki516@gmail.com>
2026-08-18 08:55:39 +0530

    724 . Find Pivot Index

---
+++ Easy/724.FindPivotIndex.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = sum(nums)
+        left_sum = 0
+        for i in range(0,len(nums)):
+            right_sum  = total - nums[i]- left_sum
+            if right_sum == left_sum :
+                return i
+            left_sum += nums[i]
+        return -1
+    
