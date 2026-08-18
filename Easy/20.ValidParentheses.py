13d28546ceba070bf61a1afa6b7c644c338b0d15
Shreyas5706 <shreyassolanki516@gmail.com>
2026-08-18 09:35:43 +0530

    20.ValidParenthesis

---
+++ Easy/20.ValidParentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+
+        for i in s:
+            if i == '(' or i == '{' or i == '[':
+                stack.append(i)
+
+            else:
+                if not stack:
+                    return False
+
+                if ((i == ')' and stack[-1] == '(') or
+                    (i == '}' and stack[-1] == '{') or
+                    (i == ']' and stack[-1] == '[')):
+
+                    stack.pop()
+                else:
+                    return False
+
+        return not stack
