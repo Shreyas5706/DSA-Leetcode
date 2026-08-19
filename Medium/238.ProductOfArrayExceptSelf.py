2d87e4508bff79d8f81e37790318dd8264fa50fb
Shreyas5706 <shreyassolanki516@gmail.com>
2026-08-19 08:42:11 +0530

    238 . Product Array Except Self

---
+++ Medium/238.ProductOfArrayExceptSelf.py
@@ -0,0 +1,19 @@
+# DOing the right and left sum one by one to reduce the tiem complexity and dont use extra space 
+
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = [1]*len(nums)
+        left = 1
+        for i in range (len(nums)):
+            answer[i]=left 
+            left*=nums[i]
+        right = 1
+        for i in range (len(nums)-1,-1,-1):
+            answer[i]*= right 
+            right*=nums[i]
+        return answer
