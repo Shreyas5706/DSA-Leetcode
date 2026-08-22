0c351fc674e715af3dd9df8fabb61a1e90ab9a06
Shreyas5706 <shreyassolanki516@gmail.com>
2026-08-22 08:25:08 +0530

    26.RemoveDuplicatedFromSortedArray

---
+++ Easy/26.RemoveDuplicatedFromSortedArray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[left - 1]:
+                nums[left] = nums[i]
+                left += 1
+
+        return left
